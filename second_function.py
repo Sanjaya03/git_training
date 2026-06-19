7706070c6323d2f3b57ab4d5b6fddb5dd07507ab
Sanjaya03 <skswain.sre@gmail.com>
2026-06-19 09:24:51 -0400

    syncing with main

--- second_function.py
+++ second_function.py
@@ -1,18 +1,8 @@
 def another_fun():
 
-<<<<<<< HEAD
-    a = 8
-    b = 5
-    return a+b
-
-
-print(f"{another_fun()}")
-
-=======
     c = 8
     d = 5
     return c+d
 
 
 print(f"{another_fun()}")
->>>>>>> 0c01d5f43f86fafa4ed1b93faab8f63538049af4
